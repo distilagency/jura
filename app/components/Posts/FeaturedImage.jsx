095672895746991d7d59d47062879f4e65669a56
Sam Logan <sam@birdbrain.com.au>
2017-02-25 16:29:16 +1100

    Starward component strip out and SASS globbing added to production

--- app/components/Posts/FeaturedImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const FeaturedImage = props => {
-  const { size, sizes, alt, url } = props;
-  const featured = sizes[size] ? sizes[size].source_url : url;
-  return (
-    <img src={featured} alt={alt} />
-  );
-};
